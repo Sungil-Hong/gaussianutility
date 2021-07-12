2457c101e39eb11e487c4541676eefaa551f7bf1
Sungil-Hong <70152068+Sungil-Hong@users.noreply.github.com>
2021-07-12 17:45:14 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gaussianutility",
+    version="0.0.1",
+    author="Sungil Hong",
+    author_email="suh33@pitt.edu",
+    description="Useful utilities to use Gaussian09 software",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sungil-Hong/gaussianutility",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy>=1.17.2']
+)
