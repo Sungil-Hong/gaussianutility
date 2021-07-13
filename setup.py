9aabca19e36dda00aada79395aad043cbca757c2
Sungil-Hong <70152068+Sungil-Hong@users.noreply.github.com>
2021-07-13 15:50:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Useful utilities to use Gaussian09 software",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    entry_points={'console_scripts': ['xgeom=gaussianutility.xgeom:main']},
+    entry_points={'console_scripts': ['exgeom=gaussianutility.exgeom:exgeom']},
     url="https://github.com/Sungil-Hong/gaussianutility",
     packages=setuptools.find_packages(),
     classifiers=[
