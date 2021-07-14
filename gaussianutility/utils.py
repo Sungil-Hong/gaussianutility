10daad28c4018db4229412ddf473e217db99bb78
Sungil-Hong <70152068+Sungil-Hong@users.noreply.github.com>
2021-07-14 17:52:17 -0400

    Update utils.py

--- gaussianutility/utils.py
+++ gaussianutility/utils.py
@@ -1,14 +1,17 @@
 #! usr/bin/env python
 
-import pandas as pd
-import sys
 import numpy as np
+import pandas as pd
 from periodictable import elements
 import ase.io
+import ase
+from ase import atoms
 import networkx as nx
 import featpro.utils as utils
+from featpro.pos import connectivity
 from itertools import islice
 
+#=======================================================================
 def extract_geom(file_name, out_file = 0):
 
     informat = file_name.rsplit(".",1)[-1]
@@ -146,30 +149,79 @@ def extract_geom(file_name, out_file = 0):
         output.write('\n')
         output.close()
 
+    
+#=======================================================================    
+def gen_atoms(file_name):
+    """Read Gaussian input (.com or .gjf), XYZ file, or Gaussian output (.out)
+    and return ASE.atoms object
+    """
+    
+    informat = file_name.rsplit(".",1)[-1]
+    
+    if informat == 'com' or informat == 'gjf' or informat == 'xyz':
+        atoms = ase.io.read(file_name)
+    elif informat == 'out':
+        try:
+            atoms = ase.io.read(file_name, format = 'gaussian-out', index=-1)
+        except StopIteration: 
+            # Sometimes, ase.io.read does not read a gaussian output file correctly.
+            # Generate an atom object manually.
+            lines = open(file_name).readlines()
+            for idx, line in enumerate(reversed(lines)):
+                if "Coordinates (Angstroms)" in line:
+                    break  
+            idx_geo = len(lines)-idx-1
+            geom = []
 
-def path_feature(file_name):
+            for line in lines[idx_geo+3:]:
+                geom.append(line.split())
+                if "----------------------------" in line:
+                    break
+
+            df_geom = pd.DataFrame(geom[:-1], columns = ['index','atomic number','atomic type','X','Y','Z'])
+
+            elem = []
+            for el in elements:
+                elem.append([el.number, el.symbol])
+
+            df_elem = pd.DataFrame(elem, columns = ['atomic number', 'symbol'])
+
+            atomNo = df_geom['atomic number']
+            atomSb = []
+            for i in atomNo:
+                atomSb.append(df_elem.loc[df_elem['atomic number'] == int(i)]['symbol'].item())
+
+            df_geom = df_geom.drop(columns=['index', 'atomic number', 'atomic type'])
+            atomPs = pd.DataFrame.to_numpy(df_geom)
+            atoms = ase.Atoms(symbols = atomSb, positions=atomPs)
+            
+    else: raise TypeError('The input file format must be .out, .com, .gjf, or .xyz')
+        
+    return atoms
+
+
+#=======================================================================
+def k_shortest_paths(G, source, target, k, weight=None):
+    return list(
+        islice(nx.shortest_simple_paths(G, source, target, weight=weight), k)
+    )
+# Sourced from https://networkx.org/documentation/stable/index.html       
+
+
+#=======================================================================
+def path_feature(atoms):
     """
     Calculate a path feature, which is defined as a weighted average of two shortest paths of every Al-Al pair.
     This feature captures the different positioning or distribution of Al in structures.
     More weight is given to the shorter path.
     
-    The input file format must be one among ".com", ".gjf", ".xyz", and ".out" (Gaussian output fule).
+    The input is ASE.atoms object
     
     Concept credit to Michael Cowan (mcowan92@gmail.com)
     """
     
-    def k_shortest_paths(G, source, target, k, weight=None):
-        return list(
-            islice(nx.shortest_simple_paths(G, source, target, weight=weight), k)
-        )
-    # Sourced from https://networkx.org/documentation/stable/index.html
+    atoms = atoms.copy()
     
-    informat = file_name.rsplit(".",1)[-1]
-    if informat == 'com' or informat == 'gjf' or informat == 'xyz':
-        atoms = ase.io.read(file_name)
-    elif informat == 'out':
-        atoms = ase.io.read(file_name, format = 'gaussian-out', index=-1)
-        
     atomSymbols = np.array(atoms.get_chemical_symbols())
     atomList = np.unique(atomSymbols, return_counts=True)
     
