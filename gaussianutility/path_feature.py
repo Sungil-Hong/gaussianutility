0c8dc6f4780fba82e670954b9cec755d45c0190c
Sungil-Hong <70152068+Sungil-Hong@users.noreply.github.com>
2021-07-14 13:09:32 -0400

    Create path_feature.py

---
+++ gaussianutility/path_feature.py
@@ -0,0 +1,55 @@
+#! usr/bin/env/python
+
+# Path feature
+#!pip3 install nerworkx
+#!pip3 install git+https://github.com/mpourmpakis/featpro.git
+
+import networkx as nx
+import featpro
+import featpro.utils as utils
+from itertools import islice
+
+# Sourced from https://networkx.org/documentation/stable/index.html
+def k_shortest_paths(G, source, target, k, weight=None):
+    return list(
+        islice(nx.shortest_simple_paths(G, source, target, weight=weight), k)
+    )
+
+def pathFeature(atoms):
+    """
+    Calculate a path feature, which is defined as a weighted average of two shortest paths of every Al-Al pair
+    This feature captures the different positioning or distribution of Al in structures
+    More weight is given to the short path
+    Arg:
+    atoms (ase.Atoms): atoms object
+    """
+    atomSymbols = np.array(atoms.get_chemical_symbols())
+    atomList = np.unique(atomSymbols, return_counts=True)
+    
+    if 'Al' not in atomSymbols: # no aluminum
+        return 'N/A'
+    elif 'Al' in atomSymbols and atomList[1][0] == 1: # single aluminum
+        return 'N/A'
+    else:
+        bonds = utils.get_bonds(atoms) # more than 1 aluminum
+
+        Al_indices = [atom.index for atom in atoms if atom.symbol == 'Al']
+        Al_first = Al_indices[0]
+        Al_others = Al_indices[1:]
+
+        G = nx.Graph()
+
+        G.add_nodes_from(np.unique(bonds))
+        G.add_edges_from(bonds)
+
+        pathLength = 0
+        for Al_target in Al_others:
+            paths = k_shortest_paths(G, Al_first, Al_target, 2)
+            #print(paths)
+            pathLength += (len(paths[0])-1)**1.5 + (len(paths[1])-1)
+
+        pathLength /= len(Al_others)
+
+        return pathLength
+
+    
