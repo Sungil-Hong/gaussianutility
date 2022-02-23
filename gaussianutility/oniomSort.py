d4621507e44b37b0de26702eccd45e07e44b2fd9
Sungil-Hong <70152068+Sungil-Hong@users.noreply.github.com>
2022-02-22 19:45:28 -0500

    Create oniomSort.py

---
+++ gaussianutility/oniomSort.py
@@ -0,0 +1,19 @@
+import click
+from gaussianutility.utils import ONIOM_sort
+
+@click.command(name = 'oniomSort',
+               context_settings = {'help_option_names': ['-h', '--help'],
+                                   'show_default': True})
+@click.argument('file_name', type=str) # it must include a file format
+
+def main(file_name, sort_idx):
+    """Sort ONIOM input file by the layer, from high to low.
+    Additional index can be 'x', 'y', 'z' (by coordinate, ascending order)
+    or 'Atom' (by atomic number, descending order)
+    Sorting by 'Atom' is defualt
+    """
+    
+    if not sort_idx:
+        sort_idx = 'Atom'
+        
+    ONIOM_sort(file_name, sort_idx)
