c5eeefda692c161778b22165a4e1abae4432e3d3
Sungil Hong <suh33@pitt.edu>
2022-02-23 18:40:18 -0500

    add click command to oniomSort.py

--- gaussianutility/oniomSort.py
+++ gaussianutility/oniomSort.py
@@ -5,6 +5,9 @@ from gaussianutility.utils import ONIOM_sort
                context_settings = {'help_option_names': ['-h', '--help'],
                                    'show_default': True})
 @click.argument('file_name', type=str) # it must include a file format
+@click.option('-i','--sort', metavar='<s>', required=False,
+             help='sort index')
+
 
 def main(file_name, sort_idx):
     """Sort ONIOM input file by the layer, from high to low.
