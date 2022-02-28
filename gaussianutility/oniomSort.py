ea9719f71604afe3cd683cb29e3c3d7cb02bfe50
Sungil-Hong <70152068+Sungil-Hong@users.noreply.github.com>
2022-02-28 11:53:32 -0500

    Update oniomSort.py
    Add freezing option

--- gaussianutility/oniomSort.py
+++ gaussianutility/oniomSort.py
@@ -6,12 +6,16 @@ from gaussianutility.utils import ONIOM_sort
                                    'show_default': True})
 @click.argument('file_name', type=str) # it must include a file format
 @click.option('-s', '--sort-idx', metavar='<s>', required=False, help='index for sorting')
+@click.option('-s'. '--freeze-idx', matevar='<s>', required=False, help='index for freezing atom position for geometry optimization')
 
-def main(file_name, sort_idx):
+def main(file_name, sort_idx, freeze_idx):
     """Sort ONIOM input file by the layer, from high to low.
-    Additional index can be 'x', 'y', 'z' (by coordinate, ascending order)
+    Additional sorting index can be 'x', 'y', 'z' (by coordinate, ascending order)
     or 'Atom' (by atomic number, descending order)
     Sorting by 'Atom' is defualt
+    
+    Additional freezing index can be 'H', 'M', 'L', or their combination
+    For example, 'ML' freezes coordinates of atoms in the middle and low ONIOM layers
     """
         
-    ONIOM_sort(file_name, sort_idx)
+    ONIOM_sort(file_name, sort_idx, freeze_idx)
