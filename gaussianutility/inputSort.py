286504147cd03cb6fe92d7dda12903dae355e234
Sungil-Hong <70152068+Sungil-Hong@users.noreply.github.com>
2022-09-05 15:49:09 -0400

    Update inputSort.py
    Remove freezing option (since there is a separate function)

--- gaussianutility/inputSort.py
+++ gaussianutility/inputSort.py
@@ -6,15 +6,10 @@ from gaussianutility.utils import input_sort
                                    'show_default': True})
 @click.argument('file_name', type=str) # it must include a file format
 @click.option('-s', '--sort-idx', metavar='<s>', required=False, help='index for sorting')
-@click.option('-f', '--freeze-idx', metavar='<s>', required=False, help='index for freezing atom position for geometry optimization')
 
-def main(file_name, sort_idx, freeze_idx):
+def main(file_name, sort_idx):
     """Sort input file by index; 'x', 'y', or 'z' coordinate, or 'Atom' symbol
     Sorting by 'Atom' is defualt
-    
-    Additional freezing index can be 'H', 'M', 'L', or their combination
-    For example, 'ML' freezes coordinates of atoms in the middle and low ONIOM layers
-    This is only for ONIOM type input structure
     """
         
-    input_sort(file_name, sort_idx, freeze_idx)
+    input_sort(file_name, sort_idx)
