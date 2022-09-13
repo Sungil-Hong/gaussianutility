9f3cf34f1821ddee32af6ac72d3e1f12aa1f1d4d
Sungil-Hong <70152068+Sungil-Hong@users.noreply.github.com>
2022-09-13 15:13:06 -0400

    Update utils.py
    Add new function com2VASP

--- gaussianutility/utils.py
+++ gaussianutility/utils.py
@@ -465,4 +465,58 @@ def freeze_layer(file_name, layer_idx = 0):
     outfile.write(''.join(rest))
     outfile.close()
 
+#=======================================================================
+def com2VASP(com_file, title = 0, lattice_param = 0):
+    '''
+    Convert .com file to .vasp file.
+    Currently, only support cubin structure, where a,b,c represent lattice parameters.
+    '''
+    if not title:
+        title = com_file
+    if not lattice_param:
+        a, b, c = 20.0168991089, 19.8754005432, 13.3344001770
+    else:
+        [a, b, c] = lattice_param
+        
+    informat, route, title_and_spin, df_geom, rest = read_input(com_file)
 
+    elem = []
+    for el in elements:
+        elem.append(el.symbol)
+
+    df_atom_sort = pd.DataFrame(elem[1:], columns = ['symbol'])
+    atom_sort_mapping = df_atom_sort.reset_index().set_index('symbol')
+    df_geom['Atomic_num'] = df_geom['Atom'].map(atom_sort_mapping['index'])
+
+    df_geom = df_geom.sort_values(by=['Atomic_num'], ascending = 0)
+    df_geom = df_geom.drop(columns=['Atomic_num'])
+    df_geom = df_geom.drop(df_geom[df_geom['Atom'] == 'Tv'].index)
+    df_geom = df_geom.drop(df_geom[df_geom['Atom'] == '?'].index)
+    
+    atomlist = np.array(df_geom['Atom'])
+    coord = df_geom[['x','y','z']]
+
+    uniqatomlist = []
+    uniqatomcount = []
+    for elem in atomlist:
+        if elem not in uniqatomlist:
+            uniqatomlist.append(elem)
+            uniqatomcount.append(np.count_nonzero(atomlist == elem))
+
+    uniqatomlist = np.array(uniqatomlist)
+    uniqatomcount= np.array(uniqatomcount, dtype=str)
+    
+    outfilename = com_file.rsplit(".",1)[0] + ".vasp"
+    outfile = open(outfilename, 'w')
+    outfile.write(title+'\n')
+    outfile.write('1.0\n')
+    outfile.write('\t'+str(a)+'\t0.0000000000\t0.0000000000\n')
+    outfile.write('\t0.0000000000\t'+str(b)+'\t0.0000000000\n')
+    outfile.write('\t0.0000000000\t0.0000000000\t'+str(c)+'\n')
+    outfile.write('\t'+'\t'.join(uniqatomlist)+'\n')
+    outfile.write('\t'+'\t'.join(uniqatomcount)+'\n')
+    outfile.write('Cartesian\n')
+    outfile.write(coord.to_csv(index=False, header=False, sep='\t'))
+    outfile.write('\n')
+    outfile.close()
+    
