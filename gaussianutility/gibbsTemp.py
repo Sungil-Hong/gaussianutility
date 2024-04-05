6d674bb7930cf93d2f5983323b6a4b9f46416e43
Sungil <suh33@pitt.edu>
2024-04-05 17:24:25 -0400

    Incorporate parser into gibbsTemp and improve readability

--- gaussianutility/gibbsTemp.py
+++ gaussianutility/gibbsTemp.py
@@ -3,6 +3,7 @@
 import numpy as np
 import math as m
 import sys
+from argparse import RawTextHelpFormatter
 
 # Thermodynamic constants
 kB = 1.380649e-23 # J/K
@@ -10,6 +11,23 @@ h = 6.62607015e-34 # Js
 Rgas = 8.3144626 # J/mol/K
 Na = 6.02214076e23
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description= "Calculated Gibbs free energy (G) at different tempeature(s) from Gaussian output file (.out)\n"
+                     "The Gaussian job must be normally terminated with frequency calculation\n"
+                     "Print the following values on terminal:\n"
+                     "   Single G, if only Temp1. is provided\n"
+                     "   List of 20 G valuse between Temp1 and Temp2 if both are provided"
+                     "   List of G values as many as step_number if Temp1, Temp2, and step_number are provided",
+        formatter_class=RawTextHelpFormatter)
+
+    parser.add_argument('file_name', help='Gaussian output file (.out)')
+    parser.add_argument('T1', type=float, help='Temperature in K to calculate G (T1)')
+    parser.add_argument('T2', type=float, nargs='?', help='Upper bound of temperature range (T2)')
+    parser.add_argument('step_number', type=int, nargs='?', default=20, help='Number of steps to calculate G', default=20)
+    args = parser.parse_args()
+    return args
+    
 # Calculate contributions from translational, rotational, vibrational, and eletronic motions
 # The equations are sourced from "Thermochemistry in Gaussian" written by Joseph W. Ochterski (2000)
 # https://gaussian.com/wp-content/uploads/dl/thermo.pdf
@@ -35,7 +53,6 @@ def Contributions(temp, mass, press, vibTemp, multiplicity, rho_r, theta_r):
     ## Vibrational part
     summ1 = 0 # for entropy contribution
     summ2 = 0 # for energy contribution
-
     for val in vibTemp:
         summ1 += val/temp/(m.exp(val/temp) - 1) - m.log(1-m.exp(-val/temp))
         summ2 += val*(1/2 + 1/(m.exp(val/temp)-1))
@@ -45,80 +62,58 @@ def Contributions(temp, mass, press, vibTemp, multiplicity, rho_r, theta_r):
     ## Electronic part
     Se = Rgas * m.log(multiplicity)
     Ee = 0
-    
     Stot = St + Sr + Sv + Se
     Etot = Et + Er + Ev + Ee
     
-    return Stot, Etot # return total entropy correction and energy correction
+    return Stot, Etot # return total entropy and energy corrections
     
-def main():
-    # Print error/help messages that looks like argparse functionality
-    if len(sys.argv) == 1:
-        print("usage: gibbsTemp [-h] file_name temp1 [temp2 [step_number]]")
-        print("gibbsTemp: error: the following arguments are required: file_name temp1")
-        sys.exit()
-
-    inFile = sys.argv[1]
-    if inFile in ['-h', '--help']:
-        print("usage: gibbsTemp [-h] file_name temp1 [temp2 [step_number]]")
-        print("\n")
-        print("Calculated Gibbs free energy at different tempeature(s) from Gaussian output file (.out)")
-        print("The Gaussian job must be normally terminated with frequency calculation\n")
-        print("Print the following values on terminal:")
-        print("     Single G value, if one temp is provided")
-        print("     List of 20 G values between temp1 and temp2 if both are provided")
-        print("     List of G values as many as step_number if temp1, temp2, and step_number are provided\n")
-        print("positional arguments:")
-        print("  file_name              Gaussian output file (.out)")
-        print("  temp1                  (starting) temperature for G calculation")
-        print("  [temp2]                End temperature for G calculation")
-        print("  [step_number]          Number of steps for G calculation bewteen temp1 and temp2")
-        print("                         Default is 20\n")
-        print("options:")
-        print("  -h, --help             show this help message and exit")
-        sys.exit()
-
+def gibbs_temp(file_name, T1, T2, step_number):
     #Set temperature based on the provided arguments
-    l = len(sys.argv)
-    if l == 2: print("Provide temperature argument(s)"); sys.exit(1)
-    elif l == 3: temperature = np.array([float(sys.argv[2])])
-    elif l == 4: temperature = np.linspace(float(sys.argv[2]), float(sys.argv[3]), 20)
-    elif l == 5: temperature = np.linspace(float(sys.argv[2]), float(sys.argv[3]), int(sys.argv[4]))
-    else: print("Too many arguments"); sys.exit(1)
+    if T2:
+        temperature = np.linspace(T1, T2, step_number)
+    else:
+        temperature = np.array(T1)
 
     # Exctract thermochemistry result from the output file
-    inFileRead = open(inFile,'r')
-    for idx, line in enumerate(inFileRead):
-        if "Thermochemistry" in line: beginIdx = idx
-        if "Total Bot" in line: endIdx = idx; break
-    inFileRead.close()
+    with open(file_name, 'r') as inFile:
+        lines = inFile.readlines()
 
-    thermochem = []   
-    inFileRead = open(inFile,'r')
-    for idx, line in enumerate(inFileRead):
-        if "Multiplicity" in line: multiplicity = int(line.split()[5]) 
-        if idx >= beginIdx-1 and idx < endIdx-3:
-            thermochem.append(line)
+    multiplicity = None
+    thermochem = []
+    collecting_thermochem = False
 
-    # Extract required informations from the thermochemistry output
+    for line in lines:
+        if "Multiplicity" in line:
+            multiplicity = int(line.split()[5]) 
+        elif "Thermochemistry" in line:
+            collecting_thermochem = True
+        elif "Total Bot" in line:
+            break
+        elif collecting_thermochem:
+            thermochem.append(line.strip())
+    
+    if not thermochem:
+        raise ValueError("Required information not found in the output file.")
+
+    # Extract required informations from the thermochemistry results
     for idx, line in enumerate(thermochem):
         if "Pressure" in line:
             press = float(line.split()[4])*101325 # Pa
-        if "Molecular mass" in line:
+        elif "Molecular mass" in line:
             mass = float(line.split()[2])*1.6605391e-27 # kg
-        if "Rotational symmetry number" in line:\
+        elif "Rotational symmetry number" in line:\
             rho_r = float(line.split()[3]) 
-        if "Rotational temperatures" in line: 
+        elif "Rotational temperatures" in line: 
             if len(line.split()) < 5: 
                 theta_r = np.array([float(line.split()[3])])
             else:
                 theta_r = np.float64(np.array(line.split()[3:6]))
-        if "Vibrational temperature" in line:
+        elif "Vibrational temperature" in line:
             vibTempBeginIdx = idx
-        if "Zero-point correction" in line: 
+        elif "Zero-point correction" in line: 
             zpCorr = float(line.split()[2])*2625.4996394799e3 # J/mol
             vibTempEndIdx = idx
-        if "Sum of electronic and zero-point Energies" in line:
+        elif "Sum of electronic and zero-point Energies" in line:
             ElectE = float(line.split()[6])*2625.4996394799e3 - zpCorr # J/mol
 
     vibTemp = []
@@ -126,12 +121,12 @@ def main():
         for elem in line.split():
             try: vibTemp.append(float(elem))
             except: ValueError
+    
     vibTemp = np.array(vibTemp)
 
     # Arrays of entropy and energy corrections in the given temperature (range)
     StotArr=[]
     EtotArr=[]
-    
     if 'rho_r' not in locals():
         rho_r = []
         theta_r = []
@@ -154,4 +149,10 @@ def main():
     print("Temperature [K]: " + str(temperature).strip('[]'))
     print("Gibbs free energy [Hartree]: " + str(Gibbs).strip('[]'))
 
+def main():
+    args = parse_args()
+    gibbs_temp(args.file_name, args.T1, args.T2, args.step_number)
 
+if __name__ == "__main__":
+    main()
+    
