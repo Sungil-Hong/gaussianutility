e524f4db08886aaf87e5e26f337d47551d399167
Sungil Hong <sungil.hong@anl.gov>
2025-02-11 11:01:13 -0600

    Improve com2vasp to address selective dynamic

--- gaussianutility/com2vasp.py
+++ gaussianutility/com2vasp.py
@@ -14,33 +14,52 @@ def parse_args():
         formatter_class=RawTextHelpFormatter)
 
     parser.add_argument("file_name", help='Gaussian input file (.com or .gjf)')
+    parser.add_argument('-c', '--ctype', \
+    help='Coordination system type; d for direct and c for cartesian', default='d')
+    
     return parser.parse_args()
 
-def com_2_vasp(file_name):
+def com_2_vasp(file_name, ctype):
+    if ctype not in ('d', 'c'):
+        raise ValueError('The coordination type must be d for direct or c for cartesian')
+
     # Read geometry from a Gaussian input file
     _, _, _, df_geom, _ = readinput(file_name)
-    df_geom = df_geom[['Atom', 'x', 'y', 'z']]
-    df_geom['Atom'] = np.array([i.rsplit("-",1)[0] for i in list(df_geom['Atom'])])
+
+    if 'Index' in df_geom.columns:
+        indices = df_geom['Index']
+        df_geom = df_geom[['Atom', 'x', 'y', 'z']]
+    else:
+        df_geom = df_geom[['Atom', 'x', 'y', 'z']]
+
     if not 'Tv' in df_geom['Atom'].values:
-        raise ValueError('The Gaussian input file does not contain lattice information')
+        raise ValueError('The provided Gaussian input file does not contain lattice information')
 
-    # Convert Gaussian input to VASP input
-    elem = [el.symbol for el in elements]
-    df_atom_sort = pd.DataFrame(elem[1:], columns = ['symbol'])
-    atom_sort_mapping = df_atom_sort.reset_index().set_index('symbol')
-    df_geom['Atomic_num'] = df_geom['Atom'].map(atom_sort_mapping['index'])
-
-    df_geom = df_geom.sort_values(by=['Atomic_num'], ascending = 0)
-    df_geom = df_geom.drop(columns=['Atomic_num'])
-
-    # Read lattice information
-    lattice = df_geom[-3:].drop(['Atom'], axis=1) 
-
+    lattice = df_geom[-3:].drop(['Atom'], axis=1)
     df_geom = df_geom.drop(df_geom[df_geom['Atom'] == 'Tv'].index)
-    df_geom = df_geom.drop(df_geom[df_geom['Atom'] == '?s'].index)
-
     atomlist = np.array(df_geom['Atom'])
-    coord = df_geom[['x','y','z']]
+        
+    if ctype == 'd':
+        cart_coords = np.array(df_geom[['x','y','z']], dtype=float)
+        lattice_inv = np.linalg.inv(np.array(lattice, dtype=float))
+        direct_coords = np.dot(cart_coords, lattice_inv)
+
+        df_geom['x'] = direct_coords[:,0]
+        df_geom['y'] = direct_coords[:,1]
+        df_geom['z'] = direct_coords[:,2]
+
+    if 'indices' in list(locals()):
+        selective = []
+        for idx in indices:
+            if idx == '0':
+                selective.append(['T', 'T', 'T'])
+            elif idx == '-1':
+                selective.append(['F', 'F', 'F'])
+
+        selective = np.array(selective)
+        df_geom['selective_x'] = selective[:,0]
+        df_geom['selective_y'] = selective[:,1]
+        df_geom['selective_z'] = selective[:,2]
 
     uniqatomlist = []
     uniqatomcount = []
@@ -56,19 +75,30 @@ def com_2_vasp(file_name):
     # Write VASP input file
     name = ".".join(file_name.rsplit(".",1)[0:-1])
     out_file = name + ".vasp"
+    df_geom = df_geom.drop(['Atom'], axis=1)
+
     with open(out_file, 'w') as output:
         output.write(name + '\n')
         output.write('1.0\n')
         output.write(lattice.to_csv(index=False, header=False, sep='\t'))
-        output.write('   '+' '.join(uniqatomlist)+'\n')
-        output.write('   '+' '.join(uniqatomcount)+'\n')
-        output.write('Cartesian\n')
-        output.write(coord.to_csv(index=False, header=False, sep='\t'))
+        output.write('\t'+'\t'.join(uniqatomlist)+'\n')
+        output.write('\t'+'\t'.join(uniqatomcount)+'\n')
+        
+        if 'indices' in list(locals()):
+            output.write('Selective dynamics\n')
+
+        if ctype == 'd':
+            output.write('Direct\n')
+        elif ctype == 'c':
+            output.write('Cartesian\n')
+            
+        output.write(df_geom.to_csv(index=False, header=False, sep='\t'))
         output.write('\n')
+        
 
 def main():
     args = parse_args()
-    com_2_vasp(args.file_name)
+    com_2_vasp(args.file_name, args.ctype)
 
 if __name__ == "__main__":
     main()
