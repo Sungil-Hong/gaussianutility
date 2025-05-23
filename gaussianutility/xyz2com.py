31622bd6d98fd94546e6b4d0db375115569d35cf
Sungil Hong <sungil.hong@anl.gov>
2025-05-23 17:24:46 -0500

    Improve xyz2com to support extended xyz file format

--- gaussianutility/xyz2com.py
+++ gaussianutility/xyz2com.py
@@ -16,33 +16,37 @@ def parse_args():
 
 def xyz_2_com(file_name):
     # Read geometry from XYZ file
-    lines = open(file_name).readlines()
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
 
-    # Find the line where the XYZ coordinates start
-    for idx, line in enumerate(lines[:]):
-        if len(line.split()) == 4:
-            try:
-                _ = [float(val.replace('n','')) for val in line.split()[1:]]
-                break
-            except ValueError:
-                continue
+    # Atoms start at line 2
+    atoms_start = 2
+    atoms_lines = [line for line in lines[atoms_start:] if len(line.split()) >= 4]
 
-    # Decide multiplicity assuming overall neutral charge
-    elemt_list = [line.split()[0] for line in lines[idx:]]
+    # Check if this is an extended XYZ file by determining whether the first atom
+    # line contains more than four tokens (atom, x, y, z)
+    is_extended = len(atoms_lines[0].split()) > 4 
+
+    # Decide multiplicity using only the element type from each atom line
+    elemt_list = [line.split()[0] for line in atoms_lines] 
     elemt_type, elemt_count = np.unique(elemt_list, return_counts=True)
     elemt_electrons = [md.element(elemt).electrons for elemt in elemt_type]
     total_electrons = sum(np.array(elemt_electrons) * elemt_count)
     multiplicity = 1 if total_electrons % 2 == 0 else 2
 
-    # Write Gaussian input file
+    # Write Gaussian input file using only the first four tokens: element, x, y, z
     name = ".".join(file_name.rsplit(".",1)[0:-1])
     out_file = name + ".com"
     with open(out_file, 'w') as output:
         output.write('# hf/3-21g\n\n')
         output.write(f'{out_file}\n\n')
         output.write(f'0 {multiplicity}\n')
-        for line in lines[idx:]:
-            output.write(line)
+        for line in atoms_lines:  # MODIFIED: iterate over atoms_lines
+            tokens = line.split()
+            if len(tokens) >= 4:
+                element = tokens[0]
+                x, y, z = tokens[1:4]
+                output.write(f' {element:2s}    {x:>15s} {y:>15s} {z:>15s}\n')
         output.write('\n')
 
 def main():
